9aeeb6736ee1fa49fa9e3fe1a882730f34e826d8
Shohei Tochiyama <s.tochiyama@hotmail.co.jp>
2019-07-30 13:34:20 +0900

    最長共通部分列をリファクタリング
    - 部分列長さと部分列実体をいずれも取得できるように
    - 文字以外の部分列にも対応

--- src/CompetitiveProgramming/Utils/LongestCommonSubsequence.cs
+++ src/CompetitiveProgramming/Utils/LongestCommonSubsequence.cs
@@ -6,20 +6,67 @@ using System.Threading.Tasks;
 
 namespace CompetitiveProgramming.Utils
 {
-    class LongestCommonSubsequence
+    /// <summary>
+    /// 最長共通部分列
+    /// </summary>
+    class LongestCommonSubsequence<T> where T : IComparable<T>
     {
+        public IList<T> X { get; private set; }
+        public IList<T> Y { get; private set; }
+        public IList<T> Lcs { get; set; }
+        int[,] Dp { get; set; }
+
+        public int Length { get { return Dp[X.Count(), Y.Count()]; } }
+
+        public LongestCommonSubsequence(IList<T> x, IList<T> y)
+        {
+            X = x;
+            Y = y;
+
+            // 計算まで済ませておく
+            LcsDP(X, Y);
+            // 実際の部分列は使わないかもしれないので、保留
+            Lcs = null;
+        }
+
         /// <summary>
-        /// LCSの途中結果から実際の文字列を復元する
+        /// LCSのテーブルを計算する
         /// </summary>
-        /// <param name="x"></param>
-        /// <param name="l"></param>
-        /// <returns></returns>
-        static string RestoreLcdString(string x, int[,] l)
+        public void LcsDP(IList<T> x, IList<T> y)
+        {
+            var m = x.Count();
+            var n = y.Count();
+
+            Dp = new int[m + 1, n + 1];
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (x[i - 1].CompareTo(y[j - 1]) == 0)
+                    {
+                        Dp[i, j] = Dp[i - 1, j - 1] + 1;
+                    }
+                    else if (Dp[i - 1, j] >= Dp[i, j - 1])
+                    {
+                        Dp[i, j] = Dp[i - 1, j];
+                    }
+                    else
+                    {
+                        Dp[i, j] = Dp[i, j - 1];
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// LCSの計算用テーブルからLCSを復元する
+        /// </summary>
+        void RestoreLcs(IList<T> x, int[,] l)
         {
             var i = l.GetLength(0) - 1;
             var j = l.GetLength(1) - 1;
 
-            var stack = new Stack<char>();
+            var stack = new Stack<T>();
             while (i > 0 && j > 0)
             {
                 if (l[i, j] == l[i, j - 1])
@@ -38,43 +85,18 @@ namespace CompetitiveProgramming.Utils
                 }
             }
 
-            return new string(stack.ToArray());
+            Lcs = stack.ToArray();
         }
 
         /// <summary>
-        /// 最長共通部分列を取得する
-        /// x:"abc" y:"axbc", return "abc"みたいなの
+        /// LCSを取得
         /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
         /// <returns></returns>
-        static string Lcs(string x, string y)
+        public IList<T> GetLcs()
         {
-            var m = x.Length;
-            var n = y.Length;
-            var c = new int[m + 1, n + 1];
+            if (Lcs == null) RestoreLcs(X, Dp);
 
-            foreach (var i in Enumerable.Range(1, m))
-            {
-                foreach (var j in Enumerable.Range(1, n))
-                {
-                    if (x[i - 1] == y[j - 1])
-                    {
-                        c[i, j] = c[i - 1, j - 1] + 1;
-                    }
-                    else if (c[i - 1, j] >= c[i, j - 1])
-                    {
-                        c[i, j] = c[i - 1, j];
-                    }
-                    else
-                    {
-                        c[i, j] = c[i, j - 1];
-                    }
-                }
-            }
-
-            // 文字列を復元する
-            return RestoreLcdString(x, c);
+            return Lcs;
         }
     }
 }
