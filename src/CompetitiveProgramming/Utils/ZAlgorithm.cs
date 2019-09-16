f7df7c41543f7f8f59f24a9d5c7e77de4d0f3402
Shohei Tochiyama <s.tochiyama@hotmail.co.jp>
2019-09-16 15:38:54 +0900

    Z-Algorithm追加

---
+++ src/CompetitiveProgramming/Utils/ZAlgorithm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// https://atcoder.jp/contests/abc141/tasks/abc141_e
+/// </summary>
+namespace CompetitiveProgramming.Utils
+{
+    /// <summary>
+    /// sとs[i:]の最長共通接頭辞の長さを記録した配列を作成する
+    /// </summary>
+    /// <remarks>
+    /// 構築 O(|S|)
+    /// </remarks>
+    static class ZAlgorithm
+    {
+        /// <summary>
+        /// LCP(0, i)を求める
+        /// </summary>
+        public static int[] Lcp(string s)
+        {
+            var n = s.Length;
+            var lcp = new int[n];
+
+            int i = 1;
+            int j = 0;
+            while (i < n)
+            {
+                while (i + j < n && s[j] == s[i + j])
+                {
+                    j++;
+                }
+                lcp[i] = j;
+                if (j == 0)
+                {
+                    i++;
+                    continue;
+                }
+
+                int k = 1;
+                while (i + k < n && k + lcp[k] < j)
+                {
+                    lcp[i + k] = lcp[k];
+                    k++;
+                }
+
+                i += k;
+                j -= k;
+            }
+
+            return lcp;
+        }
+    }
+}
