7239a7125b93531c41a925086571951cbef84596
Shohei Tochiyama <s.tochiyama@hotmail.co.jp>
2019-10-02 00:45:57 +0900

    スターリンソートを追加しました
    幸福は義務です

---
+++ src/CompetitiveProgramming/Utils/StalinSort.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace CompetitiveProgramming.Utils
+{
+    /// <summary>
+    /// スターリンソート
+    /// </summary>
+    public static class StalinSort
+    {
+        /// <summary>
+        /// スターリンソート
+        /// 計算量O(n)の画期的なソートです
+        /// </summary>
+        /// <returns>list.Count()との比較結果は不定です</returns>
+        public static List<T> Sort<T>(IReadOnlyList<T> list, Comparison<T> comparison = null)
+        {
+            var dest = new List<T>();
+
+            if (!list.Any())
+            {
+                return dest;
+            }
+
+            if (comparison==null)
+            {
+                comparison = Comparer<T>.Default.Compare;
+            }
+
+            dest.Add(list[0]);
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (comparison(dest.Last(), list[i]) >= 0)
+                {
+                    dest.Add(list[i]);
+                }
+            }
+
+            return dest;
+        }
+    }
+}
