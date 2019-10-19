768516b07d452ae5321540f18b1439abb31298d3
Shohei Tochiyama <s.tochiyama@hotmail.co.jp>
2019-10-20 00:58:00 +0900

    最短経路手直し
    - ワーシャルフロイドに経路復元を追加
    - ノード間で経路を有するかはメソッド経由で取得するように

--- src/CompetitiveProgramming/Utils/ShortestPath.cs
+++ src/CompetitiveProgramming/Utils/ShortestPath.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 /// BellmanFord(負の経路 簡略）: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_B&lang=jp
 /// BellmanFord(負の経路 詳細）: https://atcoder.jp/contests/abc137/tasks/abc137_e
 /// WarshallFloyd: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_C&lang=jp
+/// 
+/// 経路復元(Dijkstra, WarshallFloyd): http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0155
 /// </summary>
 namespace CompetitiveProgramming.Utils
 {
@@ -18,6 +20,102 @@ namespace CompetitiveProgramming.Utils
     /// </summary>
     class Dijkstra
     {
+        const long Inf = long.MaxValue / 3;
+
+        // ノード数
+        readonly int V;
+        // 隣接リスト
+        readonly List<List<Edge>> Edges;
+        // 前回の最短経路結果
+        readonly long[] Dist;
+        // 経路復元用情報
+        readonly int[] Prev;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="v">ノード数</param>
+        public Dijkstra(int v)
+        {
+            Edges = Enumerable.Repeat(0, v)
+                .Select(_ => new List<Edge>())
+                .ToList();
+            Dist = new long[v];
+            Prev = new int[v];
+
+            V = v;
+        }
+
+        /// <summary>
+        /// 経路追加
+        /// </summary>
+        public void AddPath(int from, int to, long cost)
+            => Edges[from].Add(new Edge(to, cost));
+
+        /// <summary>
+        /// sから各ノードへの最短経路
+        /// </summary>
+        public long[] ShortestPath(int s)
+        {
+            for (int i = 0; i < V; i++)
+            {
+                Dist[i] = Inf;
+                Prev[i] = -1;
+            }
+
+            // 昇順ソートにする
+            var pq = new PriorityQueue<Edge>((x, y) => y.CompareTo(x));
+
+            Dist[s] = 0;
+            pq.Push(new Edge(s, 0));
+
+            while (pq.Any())
+            {
+                var p = pq.Pop();
+                var v = p.To;
+
+                // 他の経路の方がコストが低い
+                if (Dist[v] < p.Cost) continue;
+
+                foreach (var e in Edges[v])
+                {
+                    if (Dist[e.To] > Dist[v] + e.Cost)
+                    {
+                        // こっちの経路の方が効率的
+                        Dist[e.To] = Dist[v] + e.Cost;
+                        Prev[e.To] = v;
+                        pq.Push(new Edge(e.To, Dist[e.To]));
+                    }
+                }
+            }
+
+            return Dist;
+        }
+
+        /// <summary>
+        /// 経路を復元する
+        /// </summary>
+        /// <param name="t">目的ノード</param>
+        /// <returns>探索したことがないときはnull</returns>
+        public int[] RestorePath(int t)
+        {
+            if (Prev == null) return null;
+
+            var stack = new Stack<int>();
+            while (t >= 0)
+            {
+                stack.Push(t);
+                t = Prev[t];
+            }
+
+            return stack.ToArray();
+        }
+
+        /// <summary>
+        /// 接続されているか?
+        /// </summary>
+        public bool IsConnected(int e) => Dist[e] != Inf;
+
         struct Edge : IComparable<Edge>
         {
             public int To;
@@ -33,88 +131,6 @@ namespace CompetitiveProgramming.Utils
             {
                 return this.Cost.CompareTo(other.Cost);
             }
-        }
-
-        public static readonly long Inf = long.MaxValue / 3;
-
-        // 接続情報
-        readonly List<List<Edge>> es;
-        // ノード数
-        readonly int V;
-        // 経路復元用情報
-        int[] Prev;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="v">ノード数</param>
-        public Dijkstra(int v)
-        {
-            es = Enumerable.Repeat(0, v)
-                .Select(_ => new List<Edge>())
-                .ToList();
-            V = v;
-        }
-
-        /// <summary>
-        /// 経路追加
-        /// </summary>
-        public void AddPath(int from, int to, long cost)
-            => es[from].Add(new Edge(to, cost));
-
-        /// <summary>
-        /// sから各ノードへの最短経路
-        /// </summary>
-        public long[] ShortestPath(int s)
-        {
-            var d = Enumerable.Repeat(Inf, V).ToArray();
-            // 昇順ソートにする
-            var pq = new PriorityQueue<Edge>((x, y) => y.CompareTo(x));
-            Prev = Enumerable.Repeat(-1, V).ToArray();
-
-            d[s] = 0;
-            pq.Push(new Edge(s, 0));
-
-            while (pq.Any())
-            {
-                var p = pq.Pop();
-                var v = p.To;
-
-                // 他の経路の方がコストが低い
-                if (d[v] < p.Cost) continue;
-
-                foreach (var e in es[v])
-                {
-                    if (d[e.To] > d[v] + e.Cost)
-                    {
-                        // こっちの経路の方が効率的
-                        d[e.To] = d[v] + e.Cost;
-                        Prev[e.To] = v;
-                        pq.Push(new Edge(e.To, d[e.To]));
-                    }
-                }
-            }
-
-            return d;
-        }
-
-        /// <summary>
-        /// 経路を復元する
-        /// </summary>
-        /// <param name="t">目的ノード</param>
-        /// <returns>探索したことがないときはnull</returns>
-        public int[] Path(int t)
-        {
-            if (this.Prev == null) return null;
-
-            var stack = new Stack<int>();
-            while (t >= 0)
-            {
-                stack.Push(t);
-                t = Prev[t];
-            }
-
-            return stack.ToArray();
         }
 
         /// <summary>
@@ -222,10 +238,115 @@ namespace CompetitiveProgramming.Utils
     /// </summary>
     class BellmanFord
     {
-        public static readonly long Inf = long.MaxValue / 3;
-        public static readonly long NegCycle = -Inf;
+        /// <summary>
+        /// 負の閉路を持つか？
+        /// </summary>
+        public bool HasNegCycle { get; private set; } = false;
 
-        public bool HasNegCycle = false;
+        const long Inf = long.MaxValue / 3;
+        const long NegCycle = -Inf;
+
+        // ノード数
+        readonly int V;
+        // 隣接リスト
+        readonly List<Edge> Edges;
+        // 前回の最短経路結果
+        readonly long[] Dist;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="v">ノード数</param>
+        public BellmanFord(int v)
+        {
+            Edges = new List<Edge>();
+            Dist = new long[v];
+            V = v;
+        }
+
+        /// <summary>
+        /// 経路追加
+        /// </summary>
+        public void AddPath(int from, int to, long cost)
+            => Edges.Add(new Edge(from, to, cost));
+
+        /// <summary>
+        /// sから各ノードへの最短経路
+        /// 負の閉路がある場合HasNegCycleがtrueになる
+        /// </summary>
+        /// <param name="s">探索始点</param>
+        /// <param name="needsDetail">負の閉路との接続をノードごとに確かめるか</param>
+        /// <remarks>
+        /// needsDetailがtrue
+        /// 負の閉路と接続されている経路情報は NegCycle となり
+        /// IsConnectedNegCycle で負の閉路との接続を確認できる
+        /// 負の閉路と無関係な経路情報を使いたいときはこっち
+        /// 
+        /// needsDetailがfalse
+        /// 負の閉路と接続されている経路情報不定のまま
+        /// </remarks>
+        public long[] ShortestPath(int s, bool needsDetail=false)
+        {
+            var E = Edges.Count();
+            for (int i = 0; i < V; i++)
+            {
+                Dist[i] = Inf;
+            }
+
+            Dist[s] = 0;
+            HasNegCycle = false;
+            int count = RelaxEdges(E, Dist, (x, y) => x + y);
+
+            // 閉路が一つでもある
+            HasNegCycle = count > V - 1;
+
+            if (needsDetail)
+            {
+                // 詳細な負の閉路検出
+                RelaxEdges(E, Dist, (x, y) => NegCycle);
+            }
+
+            return Dist;
+        }
+
+        /// <summary>
+        /// 辺の緩和
+        /// </summary>
+        int RelaxEdges(int E, long[] d, Func<long, long, long> relax)
+        {
+            int count;
+            for (count = 0; count < V; count++)
+            {
+                var update = false;
+                for (int i = 0; i < E; i++)
+                {
+                    var e = Edges[i];
+                    if (d[e.From] == Inf) continue;
+                    if (d[e.To] > d[e.From] + e.Cost)
+                    {
+                        d[e.To] = relax(d[e.From], e.Cost);
+                        update = true;
+                    }
+                }
+
+                if (!update) break;
+            }
+
+            return count;
+        }
+
+        /// <summary>
+        /// 接続されているか?
+        /// </summary>
+        public bool IsConnected(int t) => Dist[t] != Inf;
+
+        /// <summary>
+        /// 負の閉路と接続されているか？
+        /// </summary>
+        /// <remarks>
+        /// ShortestPath で needsDetail=false を指定した場合は使えない
+        /// </remarks>
+        public bool IsConnectedNegCycle(int t) => Dist[t] == NegCycle;
 
         struct Edge
         {
@@ -240,84 +361,6 @@ namespace CompetitiveProgramming.Utils
                 Cost = cost;
             }
         }
-
-        // 接続情報
-        readonly List<Edge> es;
-        // ノード数
-        readonly int V;
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="v">ノード数</param>
-        public BellmanFord(int v)
-        {
-            es = new List<Edge>();
-            V = v;
-        }
-
-        /// <summary>
-        /// 経路追加
-        /// </summary>
-        public void AddPath(int from, int to, long cost)
-            => es.Add(new Edge(from, to, cost));
-
-        /// <summary>
-        /// sから各ノードへの最短経路
-        /// 負の経路がある場合HasNegCycleがtrueになる
-        /// 
-        /// needsDetailがtrue
-        /// sとの経路に負の閉路があるノードはNegCycle
-        /// 
-        /// needsDetailがfalse
-        /// 負の経路がある辺の情報は不確かのまま
-        /// </summary>
-        public long[] ShortestPath(int s, bool needsDetail)
-        {
-            var E = es.Count();
-            var d = Enumerable.Repeat(Inf, V).ToArray();
-
-            d[s] = 0;
-            HasNegCycle = false;
-            int count = RelaxEdges(E, d, (x, y) => x + y);
-
-            // 閉路が一つでもある
-            HasNegCycle = count > V - 1;
-
-            if (needsDetail)
-            {
-                // 詳細な負の閉路検出
-                RelaxEdges(E, d, (x, y) => NegCycle);
-            }
-
-            return d;
-        }
-
-        /// <summary>
-        /// 辺の緩和
-        /// </summary>
-        int RelaxEdges(int E, long[] d, Func<long, long, long> relax)
-        {
-            int count;
-            for (count = 0; count < V; count++)
-            {
-                var update = false;
-                for (int i = 0; i < E; i++)
-                {
-                    var e = es[i];
-                    if (d[e.From] == Inf) continue;
-                    if (d[e.To] > d[e.From] + e.Cost)
-                    {
-                        d[e.To] = relax(d[e.From], e.Cost);
-                        update = true;
-                    }
-                }
-
-                if (!update) break;
-            }
-
-            return count;
-        }
     }
 
     /// <summary>
@@ -325,12 +368,21 @@ namespace CompetitiveProgramming.Utils
     /// </summary>
     class WarshallFloyd
     {
-        public static readonly long Inf = long.MaxValue / 3;
+        /// <summary>
+        /// 負の閉路を持つか？
+        /// </summary>
+        public bool HasNegCycle { get; private set; } = false;
 
-        // 接続情報
-        readonly long[,] es;
+        const long Inf = long.MaxValue / 3;
+
         // ノード数
         readonly int V;
+        // 隣接行列
+        readonly long[,] Matrix;
+        // 前回の最短経路結果
+        readonly long[,] Dist;
+        // 経路復元用
+        readonly int[,] Next;
 
         /// <summary>
         /// 
@@ -338,38 +390,36 @@ namespace CompetitiveProgramming.Utils
         /// <param name="v">ノード数</param>
         public WarshallFloyd(int v)
         {
-            es = new long[v, v];
+            Matrix = new long[v, v];
+            Next = new int[v, v];
+            Dist = new long[v, v];
             for (int i = 0; i < v; i++)
             {
                 for (int j = 0; j < v; j++)
                 {
-                    es[i, j] = (i == j) ? 0 : Inf;
+                    Matrix[i, j] = (i == j) ? 0 : Inf;
+                    Next[i, j] = j;
                 }
             }
 
             V = v;
+            Dist[0, 0] = Inf;
         }
 
         /// <summary>
         /// 経路追加
         /// </summary>
         public void AddPath(int from, int to, long cost)
-            => es[from, to] = cost;
+            => Matrix[from, to] = cost;
 
         /// <summary>
         /// sから各ノードへの最短経路
-        /// 負の閉路を持つ場合、nullを返す
+        /// 負の閉路がある場合HasNegCycleがtrueになる
         /// </summary>
         public long[,] ShortestPath()
         {
-            var d = new long[V, V];
-            for (int i = 0; i < V; i++)
-            {
-                for (int j = 0; j < V; j++)
-                {
-                    d[i, j] = es[i, j];
-                }
-            }
+            Array.Copy(Matrix, Dist, Matrix.Length);
+            HasNegCycle = false;
 
             for (int i = 0; i < V; i++)
             {
@@ -377,9 +427,11 @@ namespace CompetitiveProgramming.Utils
                 {
                     for (int k = 0; k < V; k++)
                     {
-                        if (d[j, i] == Inf || d[i, k] == Inf) continue;
+                        if (Dist[j, i] == Inf || Dist[i, k] == Inf) continue;
+                        if (Dist[j, k] <= Dist[j, i] + Dist[i, k]) continue;
 
-                        d[j, k] = Math.Min(d[j, k], d[j, i] + d[i, k]);
+                        Dist[j, k] = Dist[j, i] + Dist[i, k];
+                        Next[j, k] = Next[j, i];
                     }
                 }
             }
@@ -387,10 +439,39 @@ namespace CompetitiveProgramming.Utils
             // 負の閉路がある？
             for (int i = 0; i < V; i++)
             {
-                if (d[i, i] < 0) return null;
+                if (Dist[i, i] < 0)
+                {
+                    HasNegCycle = true;
+                    break;
+                }
             }
 
-            return d;
+            return Dist;
         }
+
+        /// <summary>
+        /// 経路復元
+        /// 探索したことがなかったり、接続されていない経路であればnull
+        /// </summary>
+        public IEnumerable<int> RestorePath(int s, int g)
+        {
+            if (Dist[0, 0] == Inf) { throw new Exception(); }
+            if (!IsConnected(s, g)) { yield break; }
+
+            var current = s;
+            while (current != g)
+            {
+                yield return current;
+
+                current = Next[current, g];
+            }
+
+            yield return current;
+        }
+
+        /// <summary>
+        /// 接続されているか?
+        /// </summary>
+        public bool IsConnected(int s, int g) => Dist[s, g] != Inf;
     }
 }
