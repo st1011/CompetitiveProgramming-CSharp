05cbf7b18dbdb943d7ad0ee357ff1b0bf2506544
Shohei Tochiyama <s.tochiyama@hotmail.co.jp>
2020-03-21 07:22:44 +0900

    二分探索へ比較関数を注入できるバージョンを追加

--- src/CompetitiveProgramming/Utils/Search.cs
+++ src/CompetitiveProgramming/Utils/Search.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 
+/// <summary>
+/// LowerBound: https://atcoder.jp/contests/abc119/tasks/abc119_d
+/// UpperBound: https://atcoder.jp/contests/abc143/tasks/abc143_d
+/// </summary>
 namespace CompetitiveProgramming.Utils
 {
     /// <summary>
-    /// 昇順ソート済みリストに対しての二分探索系
+    /// 二分探索系
     /// </summary>
     static class Search
     {
@@ -35,82 +39,133 @@ namespace CompetitiveProgramming.Utils
         /// v以上で最小のインデックスを返す
         /// </summary>
         public static int LowerBound<T>(IList<T> list, T v, int begin, int end) where T : IComparable<T>
-            => FindBound(list, v, begin, end, (x, y) => x.CompareTo(y) <= 0);
+        {
+            if (list == null) { throw new ArgumentNullException(nameof(list)); }
+            return FindBound(list, v, begin, end, (x, y) => x.CompareTo(y) <= 0);
+        }
 
         /// <summary>
         /// v以上で最小のインデックスを返す
         /// </summary>
         public static int LowerBound<T>(IList<T> list, T v) where T : IComparable<T>
-            => LowerBound<T>(list, v, 0, list.Count);
+        {
+            if (list == null) { throw new ArgumentNullException(nameof(list)); }
+            return LowerBound(list, v, 0, list.Count);
+        }
 
         /// <summary>
         /// vより大きな最小のインデックスを返す
         /// </summary>
         public static int UpperBound<T>(IList<T> list, T v, int begin, int end) where T : IComparable<T>
-            => FindBound(list, v, begin, end, (x, y) => x.CompareTo(y) < 0);
+        {
+            if (list == null) { throw new ArgumentNullException(nameof(list)); }
+            return FindBound(list, v, begin, end, (x, y) => x.CompareTo(y) < 0);
+        }
 
         /// <summary>
         /// vより大きな最小のインデックスを返す
         /// </summary>
         public static int UpperBound<T>(IList<T> list, T v) where T : IComparable<T>
-            => UpperBound(list, v, 0, list.Count);
+        {
+            if (list == null) { throw new ArgumentNullException(nameof(list)); }
+            return UpperBound(list, v, 0, list.Count);
+        }
+    }
+
+    /// <summary>
+    /// 二分探索系
+    /// 比較処理を外部から注入する用
+    /// </summary>
+    public class SearchOfComparerChangeable<T>
+    {
+        /// <summary> 比較用関数 </summary>
+        private Comparison<T> Comparison { get; set; }
+
+        public SearchOfComparerChangeable(Comparison<T> comparison = null)
+        {
+            Comparison = comparison ?? Comparer<T>.Default.Compare;
+        }
+
+        /// <summary>
+        /// Bound系の下請け
+        /// </summary>
+        static int FindBound(IList<T> list, T v, int begin, int end, Func<T, T, bool> judge)
+        {
+            int low = begin, high = end;
+
+            if (low > high)
+            {
+                throw new ArgumentException($"{nameof(low)} greater than {nameof(high)}", $"{nameof(low)} and {nameof(high)}");
+            }
+
+            while (low < high)
+            {
+                var mid = (high + low) / 2;
+
+                if (judge(v, list[mid])) { high = mid; }
+                else { low = mid + 1; }
+            }
+
+            return low;
+        }
+
+        /// <summary>
+        /// v以上の中で最小のインデックスを返す
+        /// </summary>
+        public int LowerBound(IList<T> list, T v, int begin, int end)
+        {
+            if (list == null) { throw new ArgumentNullException(nameof(list)); }
+            return FindBound(list, v, begin, end, (x, y) => Comparison(x, y) <= 0);
+        }
+
+        /// <summary>
+        /// v以上の中で最小のインデックスを返す
+        /// </summary>
+        public int LowerBound(IList<T> list, T v)
+        {
+            if (list == null) { throw new ArgumentNullException(nameof(list)); }
+            return LowerBound(list, v, 0, list.Count);
+        }
+
+        /// <summary>
+        /// vより大きい中で最小のインデックスを返す
+        /// </summary>
+        public int UpperBound(IList<T> list, T v, int begin, int end)
+        {
+            if (list == null) { throw new ArgumentNullException(nameof(list)); }
+            return FindBound(list, v, begin, end, (x, y) => Comparison(x, y) < 0);
+        }
+
+        /// <summary>
+        /// vより大きい中で最小のインデックスを返す
+        /// </summary>
+        public int UpperBound(IList<T> list, T v)
+        {
+            if (list == null) { throw new ArgumentNullException(nameof(list)); }
+            return UpperBound(list, v, 0, list.Count);
+        }
 
         /// <summary>
         /// 範囲中でvと等価な個数を返す
         /// </summary>
-        public static int EqualRange<T>(IList<T> list, T v, int begin, int end) where T : IComparable<T>
-            => UpperBound(list, v, begin, end) - LowerBound(list, v, begin, end);
-
-        /// <summary>
-        /// 範囲中でvと等価な個数を返す
-        /// </summary>
-        public static int EqualRange<T>(IList<T> list, T v) where T : IComparable<T>
-            => UpperBound(list, v) - LowerBound(list, v);
-
-        /// <summary>
-        /// v以下で最大のインデックスを返す
-        /// O(logN+N)なので注意
-        /// </summary>
-        public static int OrLess<T>(IList<T> list, T v, int begin, int end) where T : IComparable<T>
+        public int EqualCount(IList<T> list, T v, int begin, int end)
         {
-            var i = LowerBound(list, v, begin, end);
-
-            if (list[i].CompareTo(v) != 0)
-            {
-                // v未満で最大の値とする
-                if (i > 0) i--;
-            }
-            else
-            {
-                // vと同じ値で、一番最後へ移動させる
-                while (i < end && list[i + 1].CompareTo(v) == 0) { i++; }
-            }
-
-            return i;
+            if (list == null) { throw new ArgumentNullException(nameof(list)); }
+            return UpperBound(list, v, begin, end) - LowerBound(list, v, begin, end);
         }
 
-        /// <summary>
-        /// v以下で最大のインデックスを返す
-        /// O(logN+N)なので注意
-        /// </summary>
-        public static int OrLess<T>(IList<T> list, T v) where T : IComparable<T>
-            => OrLess(list, v, 0, list.Count);
+        ///// <summary>
+        ///// v以下で最大のインデックスを返す
+        ///// リストに含まれる最小値より小さな値を指定した場合は-1を返す
+        ///// </summary>
+        //int OrLess(IList<int> list, int v, int begin, int end)
+        //    => new SearchOfComparerChangeable<int>().UpperBound(list, v, begin, end) - 1;
 
-        /// <summary>
-        /// v未満で最大のインデックスを返す
-        /// </summary>
-        public static int LessThan<T>(IList<T> list, T v, int begin, int end) where T : IComparable<T>
-        {
-            var i = LowerBound(list, v, begin, end);
-
-            // v未満で最大の値とする
-            return (i > 0) ? i - 1 : i;
-        }
-
-        /// <summary>
-        /// v以下で最大のインデックスを返す
-        /// </summary>
-        public static int LessThan<T>(IList<T> list, T v) where T : IComparable<T>
-            => LessThan(list, v, 0, list.Count);
+        ///// <summary>
+        ///// v未満で最大のインデックスを返す
+        ///// リストに含まれる最小値より小さな値を指定した場合は-1を返す
+        ///// </summary>
+        //int LessThan(IList<T> list, T v, int begin, int end)
+        //    => LowerBound(list, v, begin, end) - 1;
     }
 }
