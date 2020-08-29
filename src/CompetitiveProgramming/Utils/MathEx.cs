69f6bc716056b4e52e1bd4abcc8d13f4da396365
Shohei Tochiyama <s.tochiyama@hotmail.co.jp>
2020-08-30 02:18:34 +0900

    ファイル構成見直し
    - `Divisor`, `GcdLcm`, `Prime`を`MathEx`へ集約

--- src/CompetitiveProgramming/Utils/MathEx.cs
+++ src/CompetitiveProgramming/Utils/MathEx.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 
 /// <summary>
+/// GCD / LCM: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0005&lang=ja
+/// ExGCD: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_E&lang=ja
+/// 
+/// 約数列挙: https://atcoder.jp/contests/abc112/tasks/abc112_d
+/// (あんまりズバリなverify用問題が無い)
+/// 
 /// 素数判定: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_1_C&lang=jp
 /// 素因数分解: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_A&lang=jp
 /// 素数列挙: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0044
@@ -12,12 +18,132 @@ using System.Linq;
 namespace CompetitiveProgramming.Utils
 {
     /// <summary>
+    /// 最大公約数
+    /// 最小公倍数
+    /// 拡張ユークリッド互除法
+    /// 
+    /// 約数列挙
+    /// 
     /// 素数判定
     /// 素因数分解
     /// 素数列挙（エラトステネスの篩）
     /// </summary>
-    public static class Prime
+    public static class MathEx
     {
+        #region GCD / LCM
+        /// <summary>
+        /// 最大公約数
+        /// ユークリッド互除法
+        /// </summary>
+        public static long Gcd(long a, long b)
+        {
+            long r;
+
+            while ((r = a % b) != 0)
+            {
+                a = b;
+                b = r;
+            }
+
+            return b;
+        }
+
+        /// <summary>
+        /// 最大公約数
+        /// </summary>
+        public static long Gcd(IEnumerable<long> nums)
+            => nums.Aggregate((n, next) => n = Gcd(n, next));
+
+        /// <summary>
+        /// 最大公約数
+        /// </summary>
+        public static int Gcd(IEnumerable<int> nums)
+            => (int)Gcd(nums.Select(x => (long)x));
+
+        /// <summary>
+        /// 最小公倍数
+        /// </summary>
+        public static long Lcm(long a, long b)
+            => a / Gcd(a, b) * b;
+
+        /// <summary>
+        /// 最小公倍数
+        /// </summary>
+        public static long Lcm(IEnumerable<long> nums)
+            => nums.Aggregate((n, next) => n = Lcm(n, next));
+
+        /// <summary>
+        /// 最小公倍数
+        /// </summary>
+        public static long Lcm(IEnumerable<int> nums)
+            => Lcm(nums.Select(x => (long)x));
+
+        /// <summary>
+        /// 拡張ユークリッドの互除法
+        /// ax+by = gcd(a,b)の整数解
+        /// </summary>
+        public static void ExtendedGcd(long a, long b, out long x, out long y)
+        {
+            long u = 0, v = 1;
+            x = 1; y = 0;
+
+            while (b > 0)
+            {
+                long q = a / b;
+                long t;
+
+                t = u;
+                u = x - q * u;
+                x = t;
+
+                t = v;
+                v = y - q * v;
+                y = t;
+
+                t = b;
+                b = a - q * b;
+                a = t;
+            }
+        }
+        #endregion
+
+        #region 約数列挙
+        /// <summary>
+        /// 約数列挙 順番はバラバラなので注意！
+        /// </summary>
+        private static IEnumerable<long> Divisors(long n)
+        {
+            if (n < 1) yield break;
+
+            var rn = (int)(Math.Sqrt(n) + 1);
+            for (long i = 1; i < rn; i++)
+            {
+                if ((n % i) == 0)
+                {
+                    yield return i;
+
+                    if (i != (n / i)) yield return n / i;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 約数列挙
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="needsSort">昇順ソートするか</param>
+        /// <returns></returns>
+        public static long[] Divisors(long n, bool needsSort = false)
+        {
+            var divs = Divisors(n).ToArray();
+
+            if (needsSort) Array.Sort(divs);
+
+            return divs;
+        }
+        #endregion
+
+        #region 素数
         /// <summary>
         /// 素数判定
         /// </summary>
@@ -70,7 +196,7 @@ namespace CompetitiveProgramming.Utils
         /// <summary>
         /// n以下の素数リスト
         /// </summary>
-        public static IEnumerable<int> List(int n)
+        public static IEnumerable<int> GetPrimeList(int n)
         {
             var sieve = Sieve(n);
 
@@ -137,5 +263,6 @@ namespace CompetitiveProgramming.Utils
 
             return count;
         }
+        #endregion
     }
 }
