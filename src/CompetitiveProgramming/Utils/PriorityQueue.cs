24cc11333e25fe4ed2be4b76d417a3bb90330d50
Shohei Tochiyama <s.tochiyama@hotmail.co.jp>
2021-04-18 02:59:38 +0900

    Priority Queue手直し

--- src/CompetitiveProgramming/Utils/PriorityQueue.cs
+++ src/CompetitiveProgramming/Utils/PriorityQueue.cs
@@ -9,8 +9,9 @@ namespace CompetitiveProgramming.Utils
 {
     /// <summary>
     /// 優先度付きキュー(二分ヒープ)
-    /// デフォルトは降順ソート
+    /// デフォルトは最大ヒープ
     /// </summary>
+    [System.Diagnostics.DebuggerDisplay("[0]={_heap[0]}, Count={Count}")]
     public class PriorityQueue<T>
     {
         private const int _defaultCapacity = 8;
@@ -27,180 +28,111 @@ namespace CompetitiveProgramming.Utils
         public PriorityQueue(int capacity) : this(capacity, Comparer<T>.Default.Compare) { }
         public PriorityQueue(Comparison<T> comparer) : this(_defaultCapacity, comparer) { }
 
-        /// <summary> 要素追加 </summary>
+        public static PriorityQueue<T> CreateMinPriorityQueue(int capacity)
+            => new PriorityQueue<T>(capacity, (x, y) => -Comparer<T>.Default.Compare(x, y));
+
+        public static PriorityQueue<T> CreateMinPriorityQueue()
+            => CreateMinPriorityQueue(_defaultCapacity);
+
+        /// <summary>
+        /// 要素追加
+        /// </summary>
         public void Enqueue(T item)
         {
-            var n = _heap.Count;
+            var child = _heap.Count;
             _heap.Add(item);
-
-            while (n > 0)
+            while (child > 0)
             {
-                var parent = (n - 1) / 2;
-                if (_comparer(_heap[parent], item) > 0) break;
+                var parent = Parent(child);
+                if (_comparer(_heap[parent], item) > 0) { break; }
 
-                _heap[n] = _heap[parent];
-                n = parent;
+                // 親子関係が正しくないので親を子側にずらす
+                _heap[child] = _heap[parent];
+                child = parent;
             }
 
-            _heap[n] = item;
+            _heap[child] = item;
         }
 
-        /// <summary> 要素追加 </summary>
-        public void Push(T item) => Enqueue(item);
-
-        /// <summary> 先頭の値を取得し削除する </summary>
+        /// <summary>
+        /// 先頭の値を取得し削除する
+        /// </summary>
         public T Dequeue()
         {
-            if (!_heap.Any())
-            {
-                throw new Exception();
-            }
-            var item = _heap[0];
-            var last = _heap.Last();
+            var rootItem = _heap[0];
 
             var n = _heap.Count - 1;
             var parent = 0;
-            var child = 2 * parent + 1;
-            while (child < n)
+            var slideItem = _heap.Last();
+            while (true)
             {
-                if (child + 1 < n && _comparer(_heap[child + 1], _heap[child]) > 0)
-                {
-                    child++;
-                }
+                int child = GetChildNearRoot(parent, n);
+                if (child < 0) { break; }
 
-                if (_comparer(last, _heap[child]) > 0) break;
+                // 根に近い方の子より、さらにlastの方が根に近いので
+                // この親にならlastを入れてOK
+                if (_comparer(slideItem, _heap[child]) > 0) { break; }
 
+                // lastより子の方が根に近かった場合は
+                // 子を親側にずらして、今ずらしたところへlastを入れられるか再探索する
                 _heap[parent] = _heap[child];
                 parent = child;
-                child = 2 * parent + 1;
             }
 
-            _heap[parent] = last;
+            _heap[parent] = slideItem;
+            // 末尾を削除する場合はコピーなどが発生しないので
+            // Listを自前実装しなくてもよい
             _heap.RemoveAt(n);
 
-            return item;
+            return rootItem;
         }
 
-        /// <summary> 先頭の値を取得し削除する </summary>
+        /// <summary>
+        /// 根に近い値を持つ子のindexを返す
+        /// </summary>
+        private int GetChildNearRoot(int parent, int n)
+        {
+            int left = Left(parent);
+            if (left >= n) { return -1; }
+
+            int right = left + 1;
+            if (right < n && _comparer(_heap[right], _heap[left]) > 0)
+            {
+                // より根に近い方の子供を選択
+                return right;
+            }
+
+            return left;
+        }
+
+        #region Alias
+        /// <summary>
+        /// 要素追加
+        /// </summary>
+        public void Push(T item) => Enqueue(item);
+
+        /// <summary>
+        /// 先頭の値を取得し削除する
+        /// </summary>
         public T Pop() => Dequeue();
+        #endregion
 
         /// <summary>
         /// 先頭の値取得（削除はしない）
         /// </summary>
-        public T Peek()
-        {
-            if (!_heap.Any()) throw new Exception();
-
-            return _heap[0];
-        }
+        public T Peek() => _heap[0];
 
         /// <summary>
         /// ヒープに格納されているアイテム数
         /// </summary>
-        public int Count() => _heap.Count;
+        public int Count { get { return _heap.Count; } }
 
         /// <summary>
         /// 一つでも値が格納されているか
         /// </summary>
         public bool Any() => _heap.Any();
-    }
 
-    /// <summary>
-    /// AIZUの教科書方式
-    /// 最初の要素数を超えられない
-    /// 上よりはちょっと早い
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class PriorityQueueAizu<T> where T : IComparable
-    {
-        private readonly T[] _heap;
-        private int _heapSize;
-        private readonly Func<T, T, bool> _compare;
-
-        public PriorityQueueAizu(int heapSize, bool isMinHeap = true)
-        {
-            // 1-originなので
-            _heap = new T[heapSize + 1];
-            _heapSize = 0;
-
-            if (isMinHeap)
-            {
-                _compare = (x, y) => x.CompareTo(y) < 0;
-            }
-            else
-            {
-                _compare = (x, y) => x.CompareTo(y) > 0;
-            }
-        }
-
-        public void Insert(T key)
-        {
-            _heapSize++;
-
-            HeapIncreaseKey(_heapSize, key);
-        }
-
-        private void HeapIncreaseKey(int index, T key)
-        {
-            _heap[index] = key;
-
-            var parent = ParentIndex(index);
-            while (index > 1 && _compare(_heap[index], _heap[parent]))
-            {
-                Swap(index, parent);
-                index = parent;
-                parent = ParentIndex(index);
-            }
-        }
-
-        public T Extract()
-        {
-            if (_heapSize < 1)
-            {
-                throw new Exception();
-            }
-
-            var max = _heap[1];
-            _heap[1] = _heap[_heapSize];
-            _heapSize--;
-            MaxHeapify(1);
-
-            return max;
-        }
-
-        public void MaxHeapify(int index)
-        {
-            var left = LeftIndex(index);
-            var right = RightIndex(index);
-
-            // 最大値のノードを選ぶ
-            int largestIndex = index;
-            if (Available(left) && _compare(_heap[left], _heap[largestIndex]))
-            {
-                largestIndex = left;
-            }
-            if (Available(right) && _compare(_heap[right], _heap[largestIndex]))
-            {
-                largestIndex = right;
-            }
-
-            if (largestIndex != index)
-            {
-                Swap(largestIndex, index);
-                MaxHeapify(largestIndex);
-            }
-        }
-
-        private void Swap(int a, int b)
-        {
-            T temp = _heap[a];
-            _heap[a] = _heap[b];
-            _heap[b] = temp;
-        }
-
-        private bool Available(int index) => index >= 1 && index <= _heapSize;
-        private static int ParentIndex(int index) => index / 2;
-        private static int LeftIndex(int index) => 2 * index;
-        private static int RightIndex(int index) => 2 * index + 1;
+        private int Left(int index) => index * 2 + 1;
+        private int Parent(int index) => (index - 1) / 2;
     }
 }
