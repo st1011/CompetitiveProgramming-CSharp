0a150d0d83799f8dea30c5df4feb898b358f78a6
Shohei Tochiyama <s.tochiyama@hotmail.co.jp>
2021-05-08 22:07:28 +0900

    順列手直し

--- src/CompetitiveProgramming/Utils/Permutation.cs
+++ src/CompetitiveProgramming/Utils/Permutation.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 
+/// <summary>
+/// https://onlinejudge.u-aizu.ac.jp/courses/lesson/8/ITP2/5/ITP2_5_C
+/// https://onlinejudge.u-aizu.ac.jp/courses/lesson/8/ITP2/5/ITP2_5_D
+/// </summary>
 namespace CompetitiveProgramming.Utils
 {
     /// <summary>
@@ -12,7 +16,7 @@ namespace CompetitiveProgramming.Utils
         /// arrayを辞書順で次の順列へ置き換える
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="array"></param>
+        /// <param name="array">対象順列 / 破壊的操作が行なわれることに注意</param>
         /// <param name="index"></param>
         /// <param name="length"></param>
         /// <param name="comp"></param>
@@ -38,14 +42,24 @@ namespace CompetitiveProgramming.Utils
         private static void Swap<T>(ref T x, ref T y) { T tmp = x; x = y; y = tmp; }
 
         /// <summary>
+        /// arrayを辞書順で前の順列へ置き換える
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array">対象順列 / 破壊的操作が行なわれることに注意</param>
+        /// <returns>前の順列が存在する場合はtrue</returns>
+        public static bool PrevPermutation<T>(T[] array) where T : IComparable<T>
+        {
+            return NextPermutation(array, 0, array.Length, (x, y) => Comparer<T>.Default.Compare(y, x));
+        }
+
+        /// <summary>
         /// arrayを辞書順で次の順列へ置き換える
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="array"></param>
+        /// <param name="array">対象順列 / 破壊的操作が行なわれることに注意</param>
         /// <returns>次の順列が存在する場合はtrue</returns>
         /// <remarks>
         /// C++のnext_permutation()のような関数
-        /// arrayは初回ソート済みで2回目以降は前回の結果をそのまま渡す
         /// </remarks>
         public static bool NextPermutation<T>(T[] array) where T : IComparable<T>
         {
@@ -53,18 +67,20 @@ namespace CompetitiveProgramming.Utils
         }
 
         /// <summary>
-        /// sourceを
+        /// arrayの辞書順順列を全列挙する
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        /// <param name="source">ソート済み配列</param>
+        /// <param name="array">昇順ソート済み順列 / 破壊的操作が行なわれることに注意</param>
         /// <returns></returns>
-        public static IEnumerable<IReadOnlyList<T>> Permutations<T>(T[] source) where T : IComparable<T>
+        /// <remarks>
+        /// 厳密には全列挙ではなく現在の順列から辞書順末尾までの順列
+        /// </remarks>
+        public static IEnumerable<IReadOnlyList<T>> Permutations<T>(T[] array) where T : IComparable<T>
         {
-            var temporary = source.Clone() as T[];
             do
             {
-                yield return temporary;
-            } while (NextPermutation(temporary));
+                yield return array;
+            } while (NextPermutation(array));
         }
     }
 }
